091f522ee9ff53a912dee47a6d08bf60f2c961dd
Matt Guthrie <matt@mkguthrie.com>
2019-07-09 13:41:16 -0700

    updated footer links

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <footer class="py-3 bg-white border-top">
         <div class="container">
-            <i class="fa fa-github-square fa-3x pr-2"></i>
-            <i class="fa fa-twitter-square fa-3x pr-2"></i>
-            <i class="fa fa-envelope-square fa-3x"></i>
+            <a href="https://github.com/guthmatt"><i class="fa fa-github-square fa-3x pr-2"></i></a>
+            <a href="https://twitter.com/m_k_guthrie"><i class="fa fa-twitter-square fa-3x pr-2"></i></a>
+            <a href="#"><i class="fa fa-envelope-square fa-3x"></i></a>
         </div>
     </footer>
-
+    
     <?php wp_footer(); ?>
 
   </body>
